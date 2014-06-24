315c3e21006bea136a2ff8c5ea3361bf996f275e
Radu Potop <radu.potop@beyondanalysis.net>
2014-06-24 17:33:46 +0100

    Added basic chat client and server broadcasting

--- client/client.js
+++ client/client.js
@@ -1,14 +1,61 @@
 // socket client demo
-var socket = io.connect('ws://localhost:3001');
+//~ var socket = io.connect('ws://localhost:3001');
 
 /**
  * Listen on event from server
  */
-socket.on('news', function(data) {
-  console.log(data);
-});
+//~ socket.on('news', function(data) {
+    //~ console.log(data);
+//~ });
 
 /**
  * Emit to client
  */
-socket.emit('client', {client: 'UA: ' + window.navigator.userAgent});
+//~ socket.emit('client', {client: 'UA: ' + window.navigator.userAgent});
+
+
+var client = angular.module('client', []);
+
+client.controller('msg', function($scope) {
+
+    /**
+     * Append message to scrollback
+     */
+    $scope.appendToScrollback = function(scrollback) {
+        angular.element('#scrollback').append(
+            angular.element(['<p>', scrollback.user, ': ', scrollback.msg, '</p>\n'].join(''))
+        );
+        //~ $scope.scrollback += [scrollback.user, ': ', scrollback.msg, '\n'].join('');$scope.scrollback
+        //~ $scope.scrollback.push(scrollback);
+        //~ console.log($scope.scrollback);
+        //~ $scope.$apply();
+    }
+
+    $scope.input = {
+        user: "The Dude"
+    }
+
+    $scope.scrollback = [];
+
+    var socket = io.connect('ws://localhost:3001');
+
+    /**
+     * Send a message
+     */
+    $scope.sengMsg = function(input) {
+
+        if(input.msg) {
+
+            socket.emit('msg', input);
+            $scope.appendToScrollback(input);
+
+            // clear msg
+            $scope.input.msg = '';
+
+        }
+
+    };
+
+    socket.on('scrollback', $scope.appendToScrollback);
+
+});
--- server/server.js
+++ server/server.js
@@ -9,26 +9,21 @@ server.listen(3001);
  * Main route
  */
 app.get('/', function(req, res) {
-  res.write('');
-  res.end();
+    res.write('');
+    res.end();
 });
 
 /**
  * Handle connection upgrade
  */
 io.on('connection', function(socket) {
-  
-  /**
-   * Emit event
-   */
-  setInterval(function() {
-    socket.emit('news', {server: 'server: ' + new Date()});
-  }, 5000);
-  
-  /**
-   * Handle data from client
-   */
-  socket.on('client', function(data) {
-    console.log(data);
-  });
+
+    /**
+     * Handle data from client
+     */
+    socket.on('msg', function(data) {
+        console.log(data);
+        socket.broadcast.emit('scrollback', data);
+    });
+
 });
