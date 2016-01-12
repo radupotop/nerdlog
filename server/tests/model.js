56651187d29a499fb049cca1554c709897bbb76f
Radu Potop <radu@wooptoo.com>
2016-01-12 15:48:10 +0000

    Added tests

---
+++ server/tests/model.js
@@ -0,0 +1,31 @@
+var chai = require('chai');
+var expect = chai.expect;
+var model = require('../model');
+
+describe('model test', function(){
+    
+    it('getUser', function(){
+        model.getUser(1, function(err, resp) {
+            expect(resp[0].user_id).to.be(1);
+        })
+    });
+
+    it('getAllBoards', function(){
+        model.getAllBoards(1, function(err, resp) {
+            expect(resp.length).to.be.greaterThan(0);
+        })
+    });
+
+    it('getAllPostsFromBoard', function(){
+        model.getAllPostsFromBoard(1, function(err, resp) {
+            expect(resp.length).to.be.greaterThan(0);
+        })
+    });
+
+    it('getPost', function(){
+        model.getPost(1, function(err, resp) {
+            expect(resp[0].post_id).to.be(1);
+        })
+    });
+
+});
